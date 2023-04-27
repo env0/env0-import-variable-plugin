b7cb802e794cf25d6901f2969176cd7fee70fc6a
Andrew Way <andrew.way@env0.com>
2023-04-26 21:37:24 -0700

    additional tests

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -1,3 +1,11 @@
 output "time" {
   value = timestamp()
 }
+
+output "time_json" {
+  value = jsonencode ({"time": output.time.value })
+}
+
+output "time_multiline" {
+  value = "This last ran on:\r\n${output.time.value}"
+}
